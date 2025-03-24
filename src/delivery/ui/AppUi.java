8226b21465c92e6391731667811ff6f739dbfd0d
hyeonji <guswl1424@gmail.com>
2025-03-24 14:43:37 +0900

    # feat: AppUi 클래스 추가

---
+++ src/delivery/ui/AppUi.java
@@ -0,0 +1,15 @@
+package delivery.ui;
+
+import java.util.Scanner;
+
+public class AppUi {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static String inputString(String message) {
+        System.out.print(message);
+        return sc.nextLine();
+    }
+
+
+}
