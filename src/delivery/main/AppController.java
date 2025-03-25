7e915ddf29110d84a17a95af191f85b42cd595aa
dojinho <20191841@kiu.kr>
2025-03-25 18:22:22 +0900

    fix: print문 바꿈

--- src/delivery/main/AppController.java
+++ src/delivery/main/AppController.java
@@ -20,12 +20,14 @@ public class AppController {
                 break;
             case 2:
                 service = new OrderService();
-            case 3:
-                service = new RiderService();
                 break;
-            case 4:
+            case 3:
                 service = new RestaurantsService();
                 break;
+            case 4:
+                service = new RiderService();
+                break;
+
             case 5:
                 System.out.println("# 프로그램을 종료합니다.");
                 System.exit(0);
