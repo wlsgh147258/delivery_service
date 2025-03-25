e48d24656b6d9c57b506f2bdb6813116fcbf63d3
hyeonji <guswl1424@gmail.com>
2025-03-25 12:24:38 +0900

    # feat: 테스트를 위한 Main, AppController 수정

--- src/delivery/main/Main.java
+++ src/delivery/main/Main.java
@@ -8,10 +8,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        RestaurantsService service = new RestaurantsService();
+
+        service.start();
+
         AppController controller = new AppController();
 
         while (true) {
-            startScreen();
+//            startScreen();
             int selectNumber = inputInteger(">>> ");
             controller.chooseSystem(selectNumber);
         }
